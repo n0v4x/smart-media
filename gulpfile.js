03d7e54e2dd46cfdefc5c73e542f439b08b2220a
AlexAvD <alex.aganow@yandex.ru>
2021-08-01 21:59:44 +0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,19 @@ const { src, series, parallel, dest, watch } = require("gulp");
 const gulpSass = require("gulp-sass")(require("sass"));
 const browserSync = require("browser-sync").create();
 const del = require("del");
+const jest = require("gulp-jest").default;
+
+const tests = () => {
+  return src("__tests__").pipe(
+    jest({
+      preprocessorIgnorePatterns: [
+        "<rootDir>/dist/",
+        "<rootDir>/node_modules/",
+      ],
+      automock: false,
+    })
+  );
+};
 
 const cleanTask = () => {
   return del("dist");
@@ -33,7 +46,8 @@ const reloadServerTask = (cb) => {
 
 const watchTask = () => {
   watch("index.html", reloadServerTask);
-  watch("src/**/*.scss", series(sassTask, reloadServerTask));
+  watch("src/**/*.scss", series(tests, sassTask, reloadServerTask));
+  watch("__tests__/**/*.scss", tests);
 };
 
 exports.default = series(cleanTask, sassTask, serveTask, watchTask);
